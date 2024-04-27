fb1a3d23ce437b590346c7431298c02bfb901d97
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-27 10:44:18 +0530

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,33 @@
-function Hero() {
-  return <div>Hero</div>;
-}
+const Hero = () => {
+  return (
+    <div className="mx-auto max-w-7xl py-10">
+      <div className="relative">
+        <img
+          src={
+            "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          alt="billboard"
+          className="h-72 w-full rounded-lg"
+          height={0}
+          width={0}
+          sizes="100vw"
+        />
+        <div className="absolute inset-0 h-full w-full rounded-lg bg-gray-950 opacity-30" />
+        <img
+          src={"../../public/assets/images/book.png"}
+          alt="billboard"
+          className="absolute bottom-0 right-5"
+          height={0}
+          width={0}
+          sizes="100vw"
+          style={{ width: "auto", height: "18rem" }}
+        />
+        <h3 className="absolute left-10 top-1/2 w-full max-w-3xl -translate-y-1/2 text-5xl font-semibold tracking-tight text-white">
+          Connect, Share and Trade Your Favourite Reads...
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
