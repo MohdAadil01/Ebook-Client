c4a728d8cd26b27f57d1fb640acec52f5442fef6
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-27 10:44:26 +0530

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="border-t p-5 text-center">
+      <span>© 2024 Mohd Aadil All rights reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
